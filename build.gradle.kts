b6c7c5a47eedf1e588277f624ebcccc4ee896645
EPG <paul.garnier@ens-rennes.fr>
2025-07-12 01:18:08 +0200

    Chunk and world save

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "org.example"
@@ -14,4 +15,26 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.14")
     implementation("io.github.classgraph:classgraph:4.8.163")
     implementation("com.google.code.gson:gson:2.10.1")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) // Minestom has a minimum Java version of 21
+    }
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "org.example.Main" // Change this to your main class
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        mergeServiceFiles()
+        archiveClassifier.set("") // Prevent the -all suffix on the shadowjar file.
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,10 @@ import net.minestom.server.entity.*;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.LightingChunk;
+import net.minestom.server.instance.anvil.AnvilLoader;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.timer.TaskSchedule;
 import org.example.items.datas.Stats;
@@ -22,11 +24,14 @@ import org.example.items.itemsList.DEV.StatsGrimoire;
 import org.example.data.JsonPlayerDataRepository;
 import org.example.data.PlayerDataService;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
         MinecraftServer server = MinecraftServer.init();
 
-        InstanceContainer instance = MinecraftServer.getInstanceManager().createInstanceContainer();
+        InstanceContainer instance = MinecraftServer.getInstanceManager().createInstanceContainer(new AnvilLoader("main_land"));
         instance.setGenerator(u -> u.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
         instance.setChunkSupplier(LightingChunk::new);
         instance.setExplosionSupplier(ExplosionSupplierUtils.DEFAULT);
@@ -62,6 +67,20 @@ public class Main {
         MojangAuth.init();
 
         //System.out.println("Items chargés : " + ItemRegistry.all().keySet());
+
+        var scheduler = MinecraftServer.getSchedulerManager();
+
+        scheduler.buildShutdownTask( () -> {
+            System.out.println("Server shutting down... Saving chunk");
+            MinecraftServer.getInstanceManager().getInstances().forEach(Instance::saveChunksToStorage);
+        });
+
+        scheduler.buildTask( () -> {
+            System.out.println("Server autosave... Saving chunk");
+            MinecraftServer.getInstanceManager().getInstances().forEach(Instance::saveChunksToStorage);
+        })      .repeat(Duration.ofSeconds(30))
+                .delay(Duration.ofMinutes(1))
+                .schedule();
 
         server.start("0.0.0.0", 25565);
     }
