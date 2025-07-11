7235c9e5919a1aa5b49a970fde953a2e0d95b595
EPG <134396078+HolyPite@users.noreply.github.com>
2025-07-11 22:38:59 +0200

    Add server shutdown utility and command

--- src/main/java/org/example/commands/CommandRegister.java
+++ src/main/java/org/example/commands/CommandRegister.java
@@ -1,10 +1,12 @@
 package org.example.commands;
 
 import net.minestom.server.MinecraftServer;
+import org.example.commands.StopCommand;
 
 public class CommandRegister {
     public static void init(){
         MinecraftServer.getCommandManager().register(new GiveItemCommand());
         MinecraftServer.getCommandManager().register(new GamemodeCommand());
+        MinecraftServer.getCommandManager().register(new StopCommand());
     };
 }
---
+++ src/main/java/org/example/commands/StopCommand.java
@@ -0,0 +1,15 @@
+package org.example.commands;
+
+import net.minestom.server.command.builder.Command;
+import org.example.utils.ServerLifecycle;
+
+/** Command to stop the Minecraft server cleanly. */
+public class StopCommand extends Command {
+    public StopCommand() {
+        super("stop", "shutdown");
+        setDefaultExecutor((sender, context) -> {
+            sender.sendMessage("Server shutting down...");
+            ServerLifecycle.shutdown();
+        });
+    }
+}
